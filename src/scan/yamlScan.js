cdbcf68b4912ea03a99e0f208cf1af42e47f6eaf
Stefan Zerkalica <zerkalica@gmail.com>
2015-07-28 13:27:15 +0300

    fix

--- src/scan/yamlScan.js
+++ src/scan/yamlScan.js
@@ -1,14 +1,14 @@
 import jsYaml from 'js-yaml'
 import os from 'os'
 
-import merge from './utils/merge'
-import scanner from './utils/scanner'
+import merge from '../utils/merge'
+import scanner from '../utils/scanner'
 
-import strategyReader from './readers/strategyReader'
-import yamlReader from './readers/yamlReader'
-import requireReader from './readers/requireReader'
+import strategyReader from '../readers/strategyReader'
+import yamlReader from '../readers/yamlReader'
+import requireReader from '../readers/requireReader'
 
-import nodeFilter from './filters/nodeFilter'
+import nodeFilter from '../filters/nodeFilter'
 
 export default function yamlScan({
     instance = 'server',
