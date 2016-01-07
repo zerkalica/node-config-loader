c281c315ca2b7800e478fa39b47766799fb05622
CTAPbIuMABP <trejgun@gmail.com>
2016-01-07 08:50:43 +0800

    good way to set default params

--- src/common/getFiles.js
+++ src/common/getFiles.js
@@ -1,10 +1,8 @@
 import globby from '../utils/globby'
 import nodeFilter from '../filters/nodeFilter'
 
-export default function getFiles(opts) {
-    /* eslint-disable no-param-reassign */
-    opts = opts || {}
-    /* eslint-enable no-param-reassign */
+export default function getFiles(opts = {}) {
+
     const {instance, env, hostname, tagSeparator} = opts
 
     return globby(opts.mask, {...opts, nodir: true})
